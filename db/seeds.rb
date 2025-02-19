329269f8ac5bb0db53658a01118703e958d1430d
mregacho29 <mregacho@rrc.ca>
2025-02-19 12:04:45 -0600

    Create a partial for displaying customer info: app/views/customers/_customer.html.erb and bulma styling in layouts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Destroy all existing records
+Customer.destroy_all
+
+# Create a new admin user if none exists
+# Run the following command to seed the database:
+# $ rails db:seed
+# This will create a new admin user if none exists in the database.
 AdminUser.create!(email: 'mregacho@rrc.ca', password: 'mypass', password_confirmation: 'mypass') if AdminUser.count == 0
+
+# Create customers
+Customer.create!(full_name: 'Ma Crizza Lynne Regacho', phone_number: '2048695137', email_address: 'mregacho@rrc.ca', notes: 'Customer Sample 1')
+Customer.create!(full_name: 'Ivana Targeryen', phone_number: '2044361256', email_address: 'ITargeryen@rrc.ca', notes: 'Customer Sample 2')
+Customer.create!(full_name: 'Diana Körbl', phone_number: '2045670237', email_address: nil, notes: 'Customer Sample 3')
+Customer.create!(full_name: 'Leonard Matthias', phone_number: '2042369862', email_address: nil, notes: 'Customer Sample 4')
+Customer.create!(full_name: 'Micha Siegward', phone_number: '2046792544', email_address: 'Msiegward@rrc.ca', notes: 'Customer Sample 5')
