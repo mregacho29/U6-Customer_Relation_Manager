28c47996e1f021fa1870bde20bcd7068d6ee3cb9
mregacho29 <mregacho@rrc.ca>
2025-02-20 01:14:04 -0600

    Create a alphabetized action/view controller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources :customers, only: [ :index ]
+  resources :customers, only: [ :index ] do
+    collection do
+      get :alphabetized
+      get :missing_email
+    end
+  end
 
   root to: "customers#index"
 
