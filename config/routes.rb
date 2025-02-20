602edfe4fff972ed9e865823bb4b3f6de520d167
mregacho29 <mregacho@rrc.ca>
2025-02-20 00:44:33 -0600

    Fix the precompilation error in manifest.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get "customers/index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  resources :customers, only: [ :index ]
+
+  root to: "customers#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
